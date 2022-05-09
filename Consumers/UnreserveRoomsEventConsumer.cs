c2ec58e60058245a5254e9527689a965c0649a2a
Mateusz <mateusz.niescier@onet.pl>
2022-05-09 18:05:38 +0200

    unreservation second part
    Signed-off-by: Mateusz <mateusz.niescier@onet.pl>

---
+++ Consumers/UnreserveRoomsEventConsumer.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using Models.Hotels;
+using Hotels.Database;
+using Hotels.Database.Tables;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Hotels.Consumers
+{
+    public class UnreserveRoomsEventConsumer : IConsumer<UnreserveRoomsEvent>
+    {
+        private readonly HotelContext hotelContext;
+        public UnreserveRoomsEventConsumer(HotelContext hotelContext)
+        {
+            this.hotelContext = hotelContext;
+        }
+
+        public async Task Consume(ConsumeContext<UnreserveRoomsEvent> taskContext)
+        {
+            Console.WriteLine(
+                $"\n\nReceived message:\n" +
+                $"ReservationNumber: {taskContext.Message.ReservationNumber},\n\n"
+            );
+            var searched_reservations = hotelContext.Reservations
+                .Where(b => b.ReservationNumber == taskContext.Message.ReservationNumber);
+            foreach (var reservation in searched_reservations)
+            {
+                hotelContext.Reservations.Remove(reservation);
+            }
+            hotelContext.SaveChanges();
+            await taskContext.Publish<UnreserveRoomsEventReply>(
+                    new UnreserveRoomsEventReply(
+                        UnreserveRoomsEventReply.State.NOT_RESERVED, 
+                        taskContext.Message.CorrelationId));
+        }
+    }
+}
